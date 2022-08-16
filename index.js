c4a3a09f8364b956bdf8ec6b843fb22617aebef7
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-16 12:02:28 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 =62;
+let num2 =1;
+let multiply =num1 * num2;
 
-// Write your code here
+let random = 10;
+random.isInteger("true");
+
+let num3 = 25;
+let num4 = 7;
+let mod = 25%7;
+
+let max = (5,10,20);
