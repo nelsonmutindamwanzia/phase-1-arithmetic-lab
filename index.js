c7e04e2f1849217179a41da4c8c526299cf3b8f9
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-16 20:45:49 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ let num1 =62;
 let num2 =1;
 let multiply =num1 * num2;
 
-let random = 10;
-random.isInteger("true");
+const random = Math.floor( Math.random() * 100 ) + 1;
 
-let num3 = 25;
-let num4 = 7;
-let mod = 25%7;
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
 
-let max = (5,10,20);
+const max = Math.max(1,5,10,15,20);
